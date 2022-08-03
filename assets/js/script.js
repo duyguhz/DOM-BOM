08b6ae460b999296928149896a778c90b20e7ae6
duyguhz <duyghuhh@code.edu.az>
2022-08-03 10:11:31 +0400

    hw

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+let div = document.createElement("div");
+div.style.height="360px";
+div.style.width="960px";
+div.innerHTML="<strong>960 x 360px</strong>";
+div.classList.add("big-div")
+
+document.body.append(div);
+
+
+
+let div2=document.createElement("div");
+div2.style.height="180px";
+div2.style.width="290px";
+div2.innerHTML="<strong>290 x 180px</strong>";
+div2.classList.add("small-div");
+document.body.append(div2);
+
+
+
+
+let title=document.createElement("p");
+title.innerHTML="<em>Picture text</em>";
+
+let text=document.createElement("p");
+text.innerHTML="Random text about the picture ";
+
+let text2=document.createElement("p");
+text2.innerText="Read More >>"
+text2.style.color="orange"
+
+
+document.body.append(title,text,text2)
+
+
+
+
+
+let div3=document.createElement("div");
+div3.style.height="180px";
+div3.style.width="290px";
+div3.innerHTML="<strong>290 x 180px</strong>";
+div3.classList.add("small-div");
+document.body.append(div3);
+
+
+let title2=document.createElement("p");
+title2.innerHTML="<em>Picture text</em>";
+
+let text3=document.createElement("p");
+text3.innerHTML="Random text about the picture ";
+
+let text4=document.createElement("p");
+text4.innerText="Read More >>"
+text4.style.color="orange"
+
+
+document.body.append(title2,text3,text4)
+
+
+
+let div4=document.createElement("div");
+div4.style.height="180px";
+div4.style.width="290px";
+div4.innerHTML="<strong>290 x 180px</strong>";
+div4.classList.add("small-div");
+document.body.append(div4);
+
+
+let title3=document.createElement("p");
+title3.innerHTML="<em>Picture text</em>";
+
+let text5=document.createElement("p");
+text5.innerHTML="Random text about the picture ";
+
+let text6=document.createElement("p");
+text6.innerText="Read More >>"
+text6.style.color="orange"
+
+
+document.body.append(title3,text5,text6)
+
+
+
+
+
+
+
+
+
+
+
+
+
